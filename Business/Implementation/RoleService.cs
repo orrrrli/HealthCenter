53b5c09bc9e6cafa974af8799057165cd887f36e
orrrrli <castaneda.kevin@uabc.edu.mx>
2023-05-01 23:00:37 -0700

    Business implementation completed

--- Business/Implementation/RoleService.cs
+++ Business/Implementation/RoleService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data.Contracts;
+using Domain.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,37 @@ using System.Threading.Tasks;
 
 namespace Business.Implementation
 {
-    internal class RoleService
+    public class RoleService : IRoleRepository
     {
+        readonly IRoleRepository _roleRepo;
+        public RoleService(IRoleRepository roleRepo)
+        {
+            _roleRepo = roleRepo;
+        }
+        public int Add(Role rol)
+        {
+            if (rol.Id <= 0) return 0;
+            if (string.IsNullOrEmpty(rol.Name)) return 0;
+            return _roleRepo.Add(rol);
+        }
+
+        public bool Delete(int id)
+        {
+            if (id <= 0) return false;
+            return (_roleRepo.Delete(id));
+        }
+
+        public Role Get(int id)
+        {
+            Role rol = _roleRepo.Get(id);
+            return rol;
+        }
+
+        public bool Update(Role rol)
+        {
+            if (rol.Id <= 0) return false;
+            if (string.IsNullOrEmpty(rol.Name)) return false;
+            return _roleRepo.Update(rol);
+        }
     }
 }
---
+++ Business/Implementation/SheetService.cs
@@ -0,0 +1,50 @@
+﻿using Business.Contracts;
+using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Implementation
+{
+    public class SheetService : ISheetService
+    {
+        private readonly ISheetService _sheetService;
+
+        public SheetService(ISheetService sheetService)
+        {
+            _sheetService = sheetService;
+        }
+        public int Add(Sheet sheet)
+        {
+            if (sheet.Id <= 0) return 0;
+            if (string.IsNullOrEmpty(sheet.Nombre)) return 0;
+            if (string.IsNullOrEmpty(sheet.Descripcion)) return 0;
+            if (string.IsNullOrEmpty(sheet.Status)) return 0;
+            return _sheetService.Add(sheet);
+        }
+
+        public Sheet Get(int id)
+        {
+            Sheet sheet = _sheetService.Get(id);
+            return sheet;
+        }
+
+        public bool Update(Sheet sheet)
+        {
+            if (sheet.Id <= 0) return false;
+            if (string.IsNullOrEmpty(sheet.Nombre)) return false;
+            if (string.IsNullOrEmpty(sheet.Descripcion)) return false;
+            if (string.IsNullOrEmpty(sheet.Status)) return false;
+            return _sheetService.Update(sheet);
+        }
+
+        public bool Delete(int id)
+        {
+            if (id <= 0) return false;
+            return (_sheetService.Delete(id));
+        }
+
+    }
+}
